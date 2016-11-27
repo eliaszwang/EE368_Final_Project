9c3baefc37430c9bc1860251c1b858b6ba2e9bb0
Eli <ewang921@gmail.com>
2016-11-27 00:11:19 -0800

    added noise to NN search

--- nearest_n.m
+++ nearest_n.m
@@ -31,6 +31,7 @@ elseif opt == 1
     RXp = Vp' * (RX-mp);
     dif = repmat(RXp, [1 size(Pp,2)]) - Pp;
     sqr = sum(dif.^2, 1);
+    sqr = sqr+0.05*min(sqr)*randn(size(sqr)); % add some noise to NN 
     [~, idx] = min(sqr);
 %     ls = mod(idx-1, (w-Q_size+1)) + 1;
 %     ks = floor((idx-1)/(w-Q_size+1)) + 1;
