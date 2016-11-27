82a5c56503cd8a9654a34c6b2a9ef4647fb8b8e5
Nicholas Tan <nt325@cornell.edu>
2016-11-26 18:28:55 -0600

    scale

--- segment.m
+++ segment.m
@@ -1,9 +1,9 @@
-function W = segment(C)
+function W = segment(C, scale)
 
 typ = 1;
 
 if typ==1
-    sigma_edge = 20; sigma_blur = 20;
+    sigma_edge = scale*20; sigma_blur = scale*20;
     BW = edge(C, 'log', 0, sigma_edge);
     W = imgaussfilt(0.5*BW, sigma_blur);
 end
